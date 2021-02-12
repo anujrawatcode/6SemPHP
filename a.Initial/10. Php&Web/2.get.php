2c9ade72c54692618ab5ac2677f30eac5f08f168
anujrawatcode <anujrawatcode@gmail.com>
2021-02-12 20:05:49 +0530

    php&Web

--- a.Initial/10. Php&Web/2.get.php
+++ a.Initial/10. Php&Web/2.get.php
@@ -15,7 +15,7 @@ print_r($_GET); // print_r  show inside array
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta  charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
